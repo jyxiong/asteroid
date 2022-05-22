2b2a28ab88cb598e26097a0744282fb61a7355b3
jyxiong <jingyu.xiong@foxmail.com>
2022-05-22 16:15:51 +0800

    添加电介质材质

--- Utility/material.h
+++ Utility/material.h
@@ -56,3 +56,40 @@ private:
     color m_albedo;
     double m_fuzzy;
 };
+
+class dielectric : public material {
+public:
+    explicit dielectric(double ir) : m_ir(ir) {}
+
+    bool scatter(const ray& r, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        // front_face: from air(1.0) to dielectric(m_ir)
+        double refraction_ratio = rec.front_face ? (1.0 / m_ir) : m_ir;
+
+        auto in_direction = unit_vector(r.direction());
+        auto cos_theta = fmin(dot(-in_direction, rec.normal), 1.0);
+        auto sin_theta = sqrt(1 - cos_theta * cos_theta);
+
+        vec3 scatter_direction;
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) // maybe not refraction
+            scatter_direction = reflect(in_direction, rec.normal);
+        else
+            scatter_direction = refract(in_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = color { 1.0, 1.0, 1.0 };
+
+        return true;
+    }
+
+private:
+    static double reflectance(double cosine, double ir) {
+        // Schlick approximation
+        auto r0 = (1 - ir) / (1.0 + ir);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow(1 - cosine, 5);
+    }
+
+private:
+    double m_ir; // index of refraction
+};
