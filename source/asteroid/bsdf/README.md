77fc0e48028b3cf3ba3da70c7fe8864f6edb9cb7
jyxiong <jingyu.xiong@foxmail.com>
2023-07-25 23:31:29 +0800

    wip: bsdf

---
+++ source/asteroid/bsdf/README.md
@@ -0,0 +1,5 @@
+https://github.com/HummaWhite/Project3-CUDA-Path-Tracer/blob/main/src/material.h
+
+https://github.com/IwakuraRein/Nagi/blob/main/src/core/material.cuh
+
+https://github.com/dw218192/Project3-CUDA-Path-Tracer/blob/main/src/interactions.h#L161
---
+++ source/asteroid/bsdf/metal.h
@@ -0,0 +1,27 @@
+#include <cuda_runtime.h>
+#include <thrust/random.h>
+#include "glm/glm.hpp"
+#include "asteroid/renderer/scene_struct.h"
+
+namespace Asteroid::Metal
+{
+
+__device__ glm::vec3 bsdf(
+    const glm::vec3 &v, const glm::vec3 &l, const Intersection &its, const Material &mtl);
+
+__device__ const glm::vec3 sampler(
+    const glm::vec3 &v,
+    const Intersection &its,
+    const Material &mtl,
+    float &pdf);
+
+__device__ float pdf(const glm::vec3 &v, const glm::vec3 &l, const Intersection &intersect, const Material &mtl);
+
+__device__ bool eval(
+    const glm::vec3 &v,
+    glm::vec3 &l,
+    glm::vec3 &eval,
+    const Intersection &intersect,
+    const Material &mtl);
+
+}
